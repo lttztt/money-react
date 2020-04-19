19f507f052f609482c5fcabaa8ec8bd9a261f737
liuzhe <zhethink@gmail.com>
2020-04-19 22:33:59 +0800

    feat: 用优雅的方式引入svg并完成Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
+require('icons/tag.svg');
+require('icons/money.svg');
+require('icons/chart.svg');
+
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -10,7 +14,14 @@ const NavWrapper = styled.nav`
     > li{
       width: 33.3333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display:flex;
+      flex-direction: column;
+      align-items: center;
+      > .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -21,12 +32,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon" fill="red">
+            <use xlinkHref="#tag" />
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money" />
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart" />
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
