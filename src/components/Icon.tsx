d3775425394cc441cf9e67b2404e65551931448b
liuzhe <zhethink@gmail.com>
2020-04-19 23:03:15 +0800

    feat: all-import引入svg

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 
-require('icons/tag.svg');
-require('icons/money.svg');
-require('icons/chart.svg');
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
 type Props = {
   name: string
