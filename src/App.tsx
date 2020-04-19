f4e05a9b55651db3b654fd5cf63c75115cbcac15
liuzhe <zhethink@gmail.com>
2020-04-19 21:20:03 +0800

    feat: 添加react-router 页面基本路由搭建

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,61 @@
-import React from 'react';
-function App() {
+import React from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      app
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Redirect exact from="/" to="/money" />
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+function NoMatch(){
+  return <h2>404</h2>
+}
 
-export default App;
+function Statistics() {
+  return <h2>统计页</h2>;
+}
+
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页
+  </h2>;
+}
