583db7e1b28ca1f9e66e37945b9a55f4e5fdccd1
liuzhe <liuzhexx@foxmail.com>
2020-06-11 08:45:47 +0800

    用非受控组件在blur的时候修改note的值

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.scss';
 
-ReactDOM.render(
-  // 这里新标签是为了提示错误的. 好用  （严格模式会出现函数组件打印两遍的问题）
-  // <React.StrictMode>
-  <App/>,
-  // </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(<App/>, document.getElementById('root')
 );
 serviceWorker.unregister();
