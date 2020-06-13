87292192bf29fe43ad90b98a985338a831c6f395
lttztt <liuzhexx@foxmail.com>
2020-06-13 21:03:14 +0800

    重构表单数据收集

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -20,6 +20,13 @@ const Money = () => {
     category: '-' as Category,
     amount: 0
   });
+  type Selected = typeof selected;
+  const onChange = (obj: Partial<Selected>) => {
+    setSelected({
+      ...selected,
+      ...obj
+    });
+  };
   return (
     <MyLayout>
       {selected.tags.join(',')}
@@ -30,32 +37,16 @@ const Money = () => {
       <hr/>
       {selected.amount}
       <TagsSection value={selected.tags}
-                   onChange={(tags) =>
-                     setSelected({
-                       ...selected,
-                       tags: tags
-                     })}/>
+                   onChange={tags => onChange({tags})}/>
       <NoteSection value={selected.note}
-                   onChange={(note) =>
-                     setSelected({
-                       ...selected,
-                       note: note
-                     })}/>
+                   onChange={note => onChange({note})}/>
       <CategorySection value={selected.category}
-                       onChange={(category) =>
-                         setSelected({
-                           ...selected,
-                           category: category
-                         })
-                       }/>
+                       onChange={category => onChange({category})}/>
       <NumberPadSection value={selected.amount}
-                        onOk={() => {}}
-                        onChange={(amount) =>
-                          setSelected({
-                            ...selected,
-                            amount: amount
-                          })
-                        }/>
+                        onChange={amount => onChange({amount})}
+                        onOk={() => {
+                        }}
+      />
     </MyLayout>
   );
 };
