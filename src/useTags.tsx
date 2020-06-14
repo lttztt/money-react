36ff00618758c4d47038932d8df7ee6f927b893e
lttztt <liuzhexx@foxmail.com>
2020-06-14 11:07:52 +0800

    创建 createId， id自增

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,12 +1,15 @@
 import {useState} from 'react';
+import {createId} from './lib/createId';
+
+const initTags = [
+  {id: createId(), name: '衣'},
+  {id: createId(), name: '食'},
+  {id: createId(), name: '住'},
+  {id: createId(), name: '行'},
+];
 
 const useTags = () => { // 封装自定义hooks
-  const [tags, setTags] = useState<{ id: number, name: string }[]>([
-    {id: 1, name: '衣'},
-    {id: 2, name: '食'},
-    {id: 3, name: '住'},
-    {id: 4, name: '行'},
-  ]);
+  const [tags, setTags] = useState<{ id: number, name: string }[]>(initTags);
   return {tags: tags, setTags};
 };
 
--- src/views/money/TagsSection.tsx
+++ src/views/money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import React from 'react';
 import useTags from '../../useTags';
+import {createId} from '../../lib/createId';
 
 const Wrapper = styled.section`
   background: #FFFFFF; padding: 12px 16px;
@@ -33,7 +34,7 @@ const TagsSection: React.FC<Props> = (props) => {
   const onAddTag = () => {
     const tagName = window.prompt('新增标签名?');
     if (tagName !== null) {
-      setTags([...tags, {id: Math.random(), name: tagName}]);
+      setTags([...tags, {id: createId(), name: tagName}]);
     }
   };
 
