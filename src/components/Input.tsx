f398d4d48288bff5d38881cc2b16ecb8b7f8dce3
lttztt <liuzhexx@foxmail.com>
2020-07-02 14:21:46 +0800

    创建单独的Input组件

---
+++ src/components/Input.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Label = styled.label`
+    display:flex;
+    align-items: center;
+    > span { margin-right: 16px; white-space: nowrap; }
+    > input {
+      display:block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+`;
+type Props = {
+  label: string;
+} & React.InputHTMLAttributes<HTMLInputElement>  // 通过类型继承 得到input元素该有的属性
+const Input: React.FC<Props> = (props) => {
+  const {label, children, ...rest} = props;  // 解构和rest方法获取, 下边使用的时候展开操作绑定到input上
+  return (
+    <Label>
+      <span>{label}</span>
+      <input {...rest} />
+    </Label>
+  );
+};
+
+export {Input};
--- src/views/money/NoteSection.tsx
+++ src/views/money/NoteSection.tsx
@@ -1,22 +1,11 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler} from 'react';
+import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
-  > label {
-    display:flex;
-    align-items: center;
-    > span { margin-right: 16px; white-space: nowrap; }
-    > input {
-      display:block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
 `;
 
 type Props = {
@@ -25,22 +14,16 @@ type Props = {
 }
 const NoteSection: React.FC<Props> = (props) => {
   const note = props.value;
-  const refInput = useRef<HTMLInputElement>(null);
-  const onBlur = () => {
-    if (refInput.current !== null) {
-      props.onChange(refInput.current.value);
-    }
+  // 这里因为函数组件不支持ref传递
+  // 非受控组件（使用defaultValue和onBlur-自己写逻辑控制的）改受控组件 （使用value和onChange react要求的)
+  const onChange:ChangeEventHandler<HTMLInputElement> = (e) => {
+    props.onChange(e.target.value)
   };
   return (
     <Wrapper>
-      <label>
-        <span>备注</span>
-        <input type="text"
-               ref={refInput}
-               defaultValue={note}
-               onBlur={onBlur}
-               placeholder="在这里添加备注"/>
-      </label>
+      <Input label="备注" type="text" placeholder="请填写备注"
+             value={note} onChange={onChange}
+      />
     </Wrapper>
   );
 };
